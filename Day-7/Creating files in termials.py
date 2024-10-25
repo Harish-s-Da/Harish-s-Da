bd23906e4cad0ba9479aea07845ff48f6e06d3d1
Harish-s-Da <srharish8599@gmail.com>
2024-10-25 23:11:57 +0530

    Add files via upload

---
+++ Day-7/Creating files in termials.py
@@ -0,0 +1,23 @@
+
+# To creat floder in bash Terminal
+# *mkdir harish 
+ 
+# To creat files in bash Terminal
+# *pwd
+
+# To creat files in  Powershell Terminal 
+# *New-Item file1.py -Itemtype file;
+
+# To open list in bash Terminal
+# *ls
+
+# To Change Directory in bash Terminal
+# *cd../ (To delet the back side path of directory)
+
+# To come out form bash Terminal
+# *>>> quit() or $ clear, (Use ctrl+c)
+
+#Meaning of three Arrows is REPL= Read-Evalute-print-loop.
+
+# To check the version in bash Terminal.
+# *$ py -3 --Version
---
+++ Day-7/Questions of terminal.py
@@ -0,0 +1,13 @@
+
+# 1. What is Terminal ?
+# Ans> Terminal is text Based Windows in to which you can type commands and views output.
+
+# 2. Waht is Bash in python ?
+# Ans> Bash is a command line interface used in many unix-like operating systems.
+
+# 3. What is Powershell in Terminal ?
+# Ans> Powershell is a scripting language desinged for system Administration and automations.
+
+# 4. Waht is the meaning of three arrows >>> ?
+# Ans> REPL= Read-Evalute-print-loop.
+
